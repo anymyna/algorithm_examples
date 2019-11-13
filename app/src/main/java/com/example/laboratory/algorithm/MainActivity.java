8ba00200de091488fa37050153f486f06b62051d
anymyna <anymyna@126.com>
2019-11-14 00:14:00 +0800

    198. 打家劫舍

--- app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
+++ app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
@@ -27,6 +27,53 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+    //c
+    public int rob(int[] nums) {
+        if(nums.length < 1)
+        {
+            return 0;
+        }
+        int temp[] = new int[nums.length];
+        if(nums.length == 1)
+        {
+            return nums[0];
+        }
+        if(nums.length == 2)
+        {
+            if( nums[0] > nums[1])
+            {
+                return nums[0];
+            }
+            else
+            {
+                return nums[1];
+            }
+        }
+
+        temp[0] = nums[0];
+        if( nums[0] > nums[1])
+        {
+            temp[1] = nums[0];
+        }
+        else
+        {
+            temp[1] = nums[1];
+        }
+
+        for(int i=2;i<nums.length;i++)
+        {
+            if(temp[i-1] > temp[i-2]+nums[i])
+            {
+                temp[i] = temp[i-1];
+            }
+            else
+            {
+                temp[i] = temp[i-2]+nums[i];
+            }
+        }
+
+        return temp[nums.length-1];
+    }
 
 //121. 买卖股票的最佳时机
     public int maxProfit(int[] prices) {
