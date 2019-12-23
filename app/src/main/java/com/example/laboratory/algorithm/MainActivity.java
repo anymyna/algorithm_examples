86cbc07056245f3540e673780b5e6f0e7ef1f1af
anymyna <anymyna@126.com>
2019-12-23 23:34:46 +0800

    819. 最常见的单词

--- app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
+++ app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -28,12 +29,78 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e(TAG," canConstruct  "+canConstruct("aa","ab"));
+        Log.e(TAG," onCreate");
 
 
 
 
 
+
+    }
+//        String paragraph = "L, P! X! C; u! P? w! P. G, S? l? X? D. w? m? f? v, x? i. z; x' m! U' M! j? V; l. S! j? r, K. O? k? p? p, H! t! z' X! v. u; F, h; s? X? K. y, Y! L; q! y? j, o? D' y? F' Z; E? W; W' W! n! p' U. N; w? V' y! Q; J, o! T? g? o! N' M? X? w! V. w? o' k. W. y, k; o' m! r; i, n. k, w; U? S? t; O' g' z. V. N? z, W? j! m? W! h; t! V' T! Z? R' w, w? y? y; O' w; r? q. G, V. x? n, Y; Q. s? S. G. f, s! U? l. o! i. L; Z' X! u. y, Q. q; Q, D; V. m. q. s? Y, U; p? u! q? h? O. W' y? Z! x! r. E, R, r' X' V, b. z, x! Q; y, g' j; j. q; W; v' X! J' H? i' o? n, Y. X! x? h? u; T? l! o? z. K' z' s; L? p? V' r. L? Y; V! V' S. t? Z' T' Y. s? i? Y! G? r; Y; T! h! K; M. k. U; A! V? R? C' x! X. M; z' V! w. N. T? Y' w? n, Z, Z? Y' R; V' f; V' I; t? X? Z; l? R, Q! Z. R. R, O. S! w; p' T. u? U! n, V, M. p? Q, O? q' t. B, k. u. H' T; T? S; Y! S! i? q! K' z' S! v; L. x; q; W? m? y, Z! x. y. j? N' R' I? r? V! Z; s, O? s; V, I, e? U' w! T? T! u; U! e? w? z; t! C! z? U, p' p! r. x; U! Z; u! j; T! X! N' F? n! P' t, X. s; q'";
+//        String banned [] = {"m","i","s","w","y","d","q","l","a","p","n","t","u","b","o","e","f","g","c","x"};
+    //mostCommonWord(paragraph,banned);
+    //819. 最常见的单词
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph = paragraph.toLowerCase();
+        paragraph = paragraph.replace("!"," ");
+        paragraph = paragraph.replace("?"," ");
+        paragraph = paragraph.replace(","," ");
+        paragraph = paragraph.replace(";"," ");
+        paragraph = paragraph.replace("."," ");
+        paragraph = paragraph.replace("'"," ");
+
+        String arr[] = paragraph.split(" ");
+
+        HashMap<String,Integer> map= new HashMap<String,Integer>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(" ".equals(arr[i]))
+            {
+                continue;
+            }
+            if(map.containsKey(arr[i]))
+            {
+                int val = map.get((arr[i]));
+                map.put(arr[i],val+1);
+            }
+            else
+            {
+                map.put(arr[i],1);
+            }
+        }
+
+        StringBuilder ban = new StringBuilder();
+        for(int i=0;i<banned.length;i++)
+        {
+            ban.append(banned[i]);
+        }
+
+        String banString = ban.toString();
+        for (Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator(); it.hasNext(); )
+        {
+            Map.Entry<String, Integer> item = it.next();
+            if(banString.contains(item.getKey()))
+            {
+                it.remove();
+            }
+        }
+
+        int count = 0;
+        String word = "";
+
+        for (Iterator<Map.Entry<String, Integer>> it = map.entrySet().iterator(); it.hasNext(); )
+        {
+            Map.Entry<String, Integer> item = it.next();
+
+            if(item.getValue() > count)
+            {
+                count = item.getValue();
+                word = item.getKey();
+            }
+        }
+
+        return word;
     }
 
     //58. 最后一个单词的长度
