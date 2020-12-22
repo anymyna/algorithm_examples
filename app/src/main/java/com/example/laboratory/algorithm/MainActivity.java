1cd3c6afee0728d39404dd1f2a368936bb98ee93
anymyna <anymyna@126.com>
2020-12-22 23:16:23 +0800

    746. 使用最小花费爬楼梯

--- app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
+++ app/src/main/java/com/example/laboratory/algorithm/MainActivity.java
@@ -48,10 +48,34 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    //746. 使用最小花费爬楼梯
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost.length < 2)
+        {
+            return 0;
+        }
 
+        int[] value = new int[cost.length+1];
+        value[0] = 0;
+        value[1] = 0;
+        for(int i=2;i<=cost.length;i++)
+        {
+            value[i] = mix(value[i-1]+cost[i-1],value[i-2]+cost[i-2]);
+        }
+        return value[cost.length];
+    }
 
-
-
+    public int mix(int a,int b)
+    {
+        if(a > b)
+        {
+            return b;
+        }
+        else
+        {
+            return a;
+        }
+    }
 
 
     public String longestPalindrome(String s) {
